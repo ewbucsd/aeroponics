0366f1a48febc4f5b1a47f2c64a8dc9214f9da93
adamango <admolani@gmail.com>
2019-03-07 21:06:55 -0800

    working on temp_sensor_6

--- arduino/temp_sensor/temp_sensor.ino
+++ arduino/temp_sensor/temp_sensor.ino
@@ -1,11 +1,24 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS_2 2
+#define ONE_WIRE_BUS_3 3
+#define ONE_WIRE_BUS_4 4
+#define ONE_WIRE_BUS_5 5
+#define ONE_WIRE_BUS_6 6
+#define ONE_WIRE_BUS_7 7
 
-OneWire oneWire(ONE_WIRE_BUS);
 
-DallasTemperature sensors(&oneWire);
+OneWire oneWire2(ONE_WIRE_BUS_2);
+OneWire oneWire3(ONE_WIRE_BUS_3);
+OneWire oneWire4(ONE_WIRE_BUS_4);
+OneWire oneWire5(ONE_WIRE_BUS_5);
+OneWire oneWire6(ONE_WIRE_BUS_6);
+OneWire oneWire7(ONE_WIRE_BUS_7);
+
+OneWire wires[6] = {oneWire2, oneWire3, oneWire4, oneWire5, oneWire6, oneWire7};
+
+DallasTemperature sensors(wires);
 
 float Celcius = 0;
 float Fahrenheit = 0;
